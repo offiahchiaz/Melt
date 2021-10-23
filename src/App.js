ef433c705b492a0f2a93b0d2590005fe3ea265cb
offiahchiaz <offiahchiaz@gmail.com>
2021-10-23 23:41:13 +0100

    Added app wide styling for background, navbar and forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/layout/Navbar";
-import Dashboard from "./components/dashboard/Dashboard";
-import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 import CreateProject from "./components/projects/CreateProject";
+import ProjectDetails from "./components/projects/ProjectDetails";
+
 
 
 class App extends Component {
