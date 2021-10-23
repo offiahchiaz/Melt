9a01202e700255b1994b611e9146f50c5c60aef6
offiahchiaz <offiahchiaz@gmail.com>
2021-10-23 06:20:08 +0100

    Added SignedInLinks and SignedOutLinks components

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignedInLinks from "./SingedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
 
 
 const Navbar = () => {
@@ -7,6 +9,8 @@ const Navbar = () => {
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to='/' className="brand-logo">MeltApp</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
             </div>
         </nav>
     )
